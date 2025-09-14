3b84869b8671855606badf41278725d77fdee62c
Daniil <daniilgricaev805@gmail.com>
2025-09-14 13:47:19 +0700

    Add new class with operation priorities

--- 1.cpp
+++ 1.cpp
@@ -2,6 +2,43 @@
 #include<string>
 #include<cctype>
 #include<vector>
+#include<map>
+
+class shuting_yard{
+    private:
+        std::map<char,int> ch;
+    public:
+        shuting_yard(){
+            ch = {
+            {'(', 0},
+            {'+', 1},
+            {'-', 1},
+            {'*', 2},
+            {'/', 2},
+            {'^', 3}
+        };
+        }
+        char get_char(std::string rpn){
+            if(rpn.empty()){
+                std::cout<<"Stack is empty!"<<std::endl;
+                return '\0';
+            }
+            if(rpn.length()==1){
+                return rpn[0];
+            }
+            for(int i = 0;i<rpn.length()-1;i++){
+                for(int j = i+1;j<rpn.length();j++){
+                    if(ch[rpn[i]]>=ch[rpn[j]]){
+                        return rpn[i];
+                    }else{
+                        return rpn[j];
+                    }
+                }
+            }
+        }
+};
+
+
 void Dijekstra_Shuting_Yard(std::string example){
     std::string rpn;
     std::vector<int>stack;
