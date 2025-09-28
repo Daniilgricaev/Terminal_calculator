35ac6958a22ee3228751fa63fd8c4dcc9d811e3d
Daniil <daniilgricaev805@gmail.com>
2025-09-28 19:18:55 +0700

    The basic logic is written
    but the code doesn't work yet.

--- 1.cpp
+++ 1.cpp
@@ -4,11 +4,11 @@
 #include<vector>
 #include<map>
 
-class shuting_yard{
+class shunting_yard{
     private:
         std::map<char,int> ch;
     public:
-        shuting_yard(){
+        shunting_yard(){
             ch = {
             {'(', 0},
             {'+', 1},
@@ -18,7 +18,7 @@ class shuting_yard{
             {'^', 3}
         };
         }
-        char get_char(std::string rpn){
+        char get_higest_priorety(std::string rpn){
             if(rpn.empty()){
                 std::cout<<"Stack is empty!"<<std::endl;
                 return '\0';
@@ -26,23 +26,19 @@ class shuting_yard{
             if(rpn.length()==1){
                 return rpn[0];
             }
-            for(int i = 0;i<rpn.length()-1;i++){
-                for(int j = i+1;j<rpn.length();j++){
-                    if(ch[rpn[i]]>=ch[rpn[j]]){
-                        return rpn[i];
-                    }else{
-                        return rpn[j];
-                    }
+            char higest = rpn[0];
+            for(int i = 0;i<rpn.length();i++){
+                if(ch[rpn[i]]>ch[higest]){
+                    higest = rpn[i];
                 }
             }
+            return higest;
         }
 };
+void String_parser(std::string example,std::string rpn,std::vector<int>stack){
+    shunting_yard yard;
+    int i = 0,j=0;
 
-
-void Dijekstra_Shuting_Yard(std::string example){
-    std::string rpn;
-    std::vector<int>stack;
-    int i = 0;
     while(example[i]!='\0'){
         if(isdigit(example[i])){
             int digit = example[i] -'0';
@@ -52,28 +48,73 @@ void Dijekstra_Shuting_Yard(std::string example){
         }
         i++;
     }
-    std::cout<<std::endl;
-    for(int i =0;i<rpn.size();i++){
-        std::cout<<rpn[i];
+    // std::cout<<std::endl;
+    // for(int i =0;i<rpn.size();i++){
+    //     std::cout<<rpn[i];
+    // }
+    // std::cout<<std::endl;
+    // for(int i = 0;i<stack.size();i++){
+    //     std::cout<<stack[i];
+    // }
+}
+void Shunting_yard_alg(std::string& rpn,std::vector<int>& stack){
+    shunting_yard yard;
+
+    auto perform_operation = [](int a,int b,char operation)->int{
+        switch(operation){
+            case '+':return a+b;
+            case '-':return a-b;
+            case '*':return a*b;
+            case '/':return a/b;
+            default:return 0;
+        }
+    };
+
+    while(!rpn.empty()){
+        char operation = yard.get_higest_priorety(rpn);
+        size_t operation_pos = -1;
+        for(int i =0;i<rpn.length();i++){
+            if(rpn[i]==operation){
+                operation_pos = i;
+                break;
+            }
+        }
+        int left = stack[operation_pos];
+        int right = stack[operation_pos+1];
+        int result = perform_operation(left,right,operation);
+
+        stack[operation_pos] = result;
+
+        for(int i = operation_pos+1;i<stack.size()-1;i++){
+            stack[i]=stack[i + 1];
+        }
+        stack.pop_back();
+        for(int i = operation_pos;i<rpn.length()-1;i++){
+            rpn[i]=rpn[i+1];
+        }
+        rpn.pop_back();
     }
-    std::cout<<std::endl;
-    for(int i = 0;i<stack.size();i++){
-        std::cout<<stack[i];
-    }
+    
 }
 int main(){
 
     std::string demo;
     std::string example;
+    std::string rpn;
+    std::vector<int>stack;
+
     std::cout<<"Enter your mathematical example"<<std::endl;
     std::getline(std::cin ,demo);
+
     for(char c:demo){
         if(c !=' '){
             example+=c;
         }
     }
-    std::cout<<example;
-    Dijekstra_Shuting_Yard(example);
 
+    std::cout<<example;
+    String_parser(example,rpn,stack);
+    Shunting_yard_alg(rpn,stack);
+    std::cout<<"Result :"<<stack[0]<<std::endl;
     return 0;
 }
